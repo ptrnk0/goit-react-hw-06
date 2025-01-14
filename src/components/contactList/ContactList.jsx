1f7fd96fb93895204d5b33f38b3d5d4a8508b6a9
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-14 20:15:33 +0200

    Implement filtering for contacts in ContactList component using Redux

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -1,17 +1,27 @@
+import { useSelector } from "react-redux";
 import Contact from "../contact/Contact";
 import css from "./ContactList.module.css";
 
-const ContactList = ({ contacts, onDeleteContact }) => {
+const getFilteredContacts = (contacts, filter) => {
+	return contacts.filter((contact) =>
+		contact.name.toLowerCase().includes(filter.toLowerCase())
+	);
+};
+
+const ContactList = () => {
+	const filter = useSelector((state) => state.filters.name);
+	const contacts = useSelector((state) => state.contacts.items);
+	const filteredContacts = getFilteredContacts(contacts, filter);
+
 	return (
 		<ul className={css.contactList}>
-			{contacts.map((item) => {
+			{filteredContacts.map((item) => {
 				return (
 					<li key={item.id}>
 						<Contact
 							contactName={item.name}
 							number={item.number}
 							id={item.id}
-							onDeleteContact={onDeleteContact}
 						/>
 					</li>
 				);
