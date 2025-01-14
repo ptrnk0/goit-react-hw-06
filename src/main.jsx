7141bc82933639aea5264b30e0f70054239e3f74
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-14 20:14:58 +0200

    Remove unused PersistGate import

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./components/app/App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 // import { persistor } from "./redux/store.js";
-import { PersistGate } from "redux-persist/integration/react";
+// import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
