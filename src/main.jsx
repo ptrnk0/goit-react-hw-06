41ae8b0ae8a8b742fb3d94359cb606810951d888
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-14 20:14:43 +0200

    Wrap App component with Redux Provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,17 @@ import { createRoot } from "react-dom/client";
 import "modern-normalize";
 import "./index.css";
 import App from "./components/app/App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+// import { persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<App />
+		<Provider store={store}>
+			{/* <PersistGate loading={<p>Loading...</p>} persistor={persistor}> */}
+			<App />
+			{/* </PersistGate> */}
+		</Provider>
 	</StrictMode>
 );
