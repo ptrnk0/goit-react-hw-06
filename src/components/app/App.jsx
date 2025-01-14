1ac9245fe737e22b9237cca13da84363d4c077e9
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-14 20:15:12 +0200

    Refactor App component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,57 +1,17 @@
 import ContactForm from "../contactForm/ContactForm";
 import SearchBox from "../searchBox/SearchBox";
 import ContactList from "../contactList/ContactList";
-import { useEffect, useState } from "react";
-import initialContacts from "../../contacts.json";
-import { nanoid } from "nanoid";
 import css from "./App.module.css";
 
 const App = () => {
-	const [searchValue, setSearchValue] = useState("");
-	const [contacts, setContacts] = useState(() => {
-		const lsContacts = window.localStorage.getItem("contacts");
-
-		if (lsContacts) {
-			return JSON.parse(lsContacts);
-		}
-
-		return initialContacts;
-	});
-
-	useEffect(() => {
-		window.localStorage.setItem("contacts", JSON.stringify(contacts));
-	}, [contacts]);
-
-	const searchContacts = contacts.filter((item) =>
-		item.name.toLowerCase().includes(searchValue.toLocaleLowerCase())
-	);
-
-	const handleAddContact = (contact) => {
-		contact.id = nanoid();
-		setContacts((prev) => {
-			return [...prev, contact];
-		});
-	};
-
-	const handleSearchContact = (value) => {
-		setSearchValue(value);
-	};
-
-	const handleDeleteContact = (contactId) => {
-		setContacts((prev) => {
-			return prev.filter((contact) => contact.id !== contactId);
-		});
-	};
-
 	return (
 		<section className={css.container}>
 			<h1 className={css.title}>Phonebook</h1>
-			<ContactForm onAddContact={handleAddContact} />
-			<SearchBox value={searchValue} onSearch={handleSearchContact} />
-			<ContactList
-				contacts={searchContacts}
-				onDeleteContact={handleDeleteContact}
-			/>
+			<ContactForm />
+			<div className={css.contactsWrapper}>
+				<SearchBox />
+				<ContactList />
+			</div>
 		</section>
 	);
 };
