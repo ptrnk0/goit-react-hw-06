e652a920e3a5a51371808720b5023d37564e311e
ptrnk0 <vlad.ptrnko@gmail.com>
2025-01-15 19:08:28 +0200

    Enable Redux persistence for contacts and integrate PersistGate in main application

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,15 @@ import "./index.css";
 import App from "./components/app/App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-// import { persistor } from "./redux/store.js";
-// import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Provider store={store}>
-			{/* <PersistGate loading={<p>Loading...</p>} persistor={persistor}> */}
-			<App />
-			{/* </PersistGate> */}
+			<PersistGate loading={<p>Loading...</p>} persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
+import {
+	persistStore,
+	persistReducer,
+	REHYDRATE,
+	PERSIST,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
 
+const persistConfig = {
+	key: "contacts",
+	storage,
+};
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
+
 export const store = configureStore({
 	reducer: {
-		contacts: contactsReducer,
+		contacts: persistedReducer,
 		filters: filtersReducer,
 	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [REHYDRATE, PERSIST],
+			},
+		}),
 });
+export const persistor = persistStore(store);
